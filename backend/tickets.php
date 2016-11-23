a9f4d6469401aa24210515dd7c7c1f4c4d75adbd
SirFaizdat <fdatoo7@gmail.com>
2016-11-23 18:40:46 -0500

    Initial commit with template

---
+++ backend/tickets.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Contains functions for managing tickets.
+ *
+ * @author Faizaan A. Datoo
+ */
+
+function give_ticket() {
+
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'backend/database.php';
+
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Family FunQuest</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Slabo+27px" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+    <!-- ADD CSS HERE -->
+</head>
+<body>
+
+<?php include 'nav.php'; ?>
+
+HOME
+
+<?php include 'footer.php'; ?>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
+<!-- ADD ANY SCRIPTS HERE -->
+</body>
+</html>
