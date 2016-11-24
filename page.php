e9e0c720571f30e6b6a17b57b63624de71c5c30a
alimo <aliimody1@gmail.com>
2016-11-24 14:21:01 -0500

    Got grids to work and show.
    Tested image replace feature (Works).
    
    HTML to be added

---
+++ page.php
@@ -0,0 +1,72 @@
+<?php
+
+require 'backend/bootstrap.php';
+
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activities List</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+    <!-- ADD CSS HERE -->
+</head>
+<body>
+
+<?php include 'nav.php'; ?>
+
+<!-- Add your stuff here -->
+
+<div class="row" style="margin: 0px" >
+    <div class="columns large-10 large-5" >
+        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
+    </div>
+</div>
+<div class="row" style="margin: 0px">
+    <div class="columns large-10 large-5">
+    <p class="callout panel" style = "display: inline-table ">.small-10 .large-5</p>
+        </div>
+</div>
+<div class="row" style="margin: 0px">
+    <div class="columns large-10 large-5">
+    <p class="callout panel" style = "display: inline-table ">.small-10 .large-5</p>
+</div> </div>
+<div class="row" style="margin: 0px">
+    <div class="columns large-10 large-5">
+    <p class="callout panel"style = "display: inline-table ">.small-10 .large-5</p>
+    </div> </div>
+<div class="row" style="margin: 0px" >
+    <div class="columns large-10 large-5" >
+        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
+    </div>
+</div>
+<div class="row" style="margin: 0px" >
+    <div class="columns large-10 large-5" >
+        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
+    </div>
+</div>
+<div class="row" style="margin: 0px" >
+    <div class="columns large-10 large-5" >
+        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
+    </div>
+</div>
+<div class="row" style="margin: 0px" >
+    <div class="columns large-10 large-5" >
+        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
+    </div>
+</div>
+<div class="row" style="margin: 0px" >
+    <div class="columns large-10 large-5" >
+        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
+    </div>
+</div>
+
+<!-- ADD ANY SCRIPTS HERE -->
+</body>
+</html>
