633fb467f3552d2f1f28b38d56322c09e6896d49
ImranD1 <idharamsi@gmail.com>
2016-11-24 14:51:14 -0500

    Activity grid set up

--- page.php
+++ page.php
@@ -16,6 +16,61 @@ require 'backend/bootstrap.php';
     <link rel="stylesheet" href="css/master.css">
     <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
     <!-- ADD CSS HERE -->
+    <style>
+        body {
+            background-color: #f8f8f8;
+            font-family: 'Raleway', sans-serif;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            font-family: 'Montserrat', sans-serif;
+        }
+
+        /* Footer */
+        .full-width {
+            max-width: 1400px;
+        }
+
+        .footer {
+            background-color: #2980b9;
+            padding: 2rem 4rem;
+            text-align: center;
+            color: #fff;
+        }
+        .footer i {
+            font-size: 100px;
+        }
+
+        .footer h4 {
+            color: #fff;
+            font-size: 1em;
+            font-weight: 400;
+            text-transform: uppercase;
+            margin-top: 2.5rem;
+            margin-bottom: 10px;
+        }
+        .footer p, .footer a {
+            font-weight: 300;
+            font-size: .8em;
+            color: #fff;
+        }
+
+        @media only screen and (min-width: 40.063em) {
+            .footer .columns:nth-child(n+2) {
+                border-left: 1px solid #b3b3b3;
+                min-height: 280px;
+            }
+        }
+        .footer-links {
+            list-style-type: none;
+        }
+        .footer-links li {
+            margin-top: .5em;
+        }
+        img{
+            padding:15px;
+        }
+    </style>
 </head>
 <body>
 
@@ -23,50 +78,53 @@ require 'backend/bootstrap.php';
 
 <!-- Add your stuff here -->
 
-<div class="row" style="margin: 0px" >
-    <div class="columns large-10 large-5" >
-        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
-    </div>
-</div>
-<div class="row" style="margin: 0px">
-    <div class="columns large-10 large-5">
-    <p class="callout panel" style = "display: inline-table ">.small-10 .large-5</p>
-        </div>
-</div>
-<div class="row" style="margin: 0px">
-    <div class="columns large-10 large-5">
-    <p class="callout panel" style = "display: inline-table ">.small-10 .large-5</p>
-</div> </div>
-<div class="row" style="margin: 0px">
-    <div class="columns large-10 large-5">
-    <p class="callout panel"style = "display: inline-table ">.small-10 .large-5</p>
-    </div> </div>
-<div class="row" style="margin: 0px" >
-    <div class="columns large-10 large-5" >
-        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
-    </div>
-</div>
-<div class="row" style="margin: 0px" >
-    <div class="columns large-10 large-5" >
-        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
-    </div>
-</div>
-<div class="row" style="margin: 0px" >
-    <div class="columns large-10 large-5" >
-        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
-    </div>
-</div>
-<div class="row" style="margin: 0px" >
-    <div class="columns large-10 large-5" >
-        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
-    </div>
-</div>
-<div class="row" style="margin: 0px" >
-    <div class="columns large-10 large-5" >
-        <p class="callout panel" style = "display: inline-table">.small-10 .large-5</p>
-    </div>
-</div>
+<div class="row" >
+    <div class="columns large-4 medium-4 small-12" >
+        <center><img src="http://placehold.it/350x150"></center>
+        <center><p>Activity 1</p></center>
 
+    </div>
+
+    <div class="columns large-4 medium-4 small-12">
+        <center><img src="http://placehold.it/350x150"></center>
+        <center><p>Activity 2</p></center>
+    </div>
+    <div class="columns large-4 medium-4 small-12">
+        <center><img src="http://placehold.it/350x150"></center>
+        <center><p>Activity 3</p></center>
+    </div>
+    </div>
+    <div class="row" >
+        <div class="columns large-4 medium-4 small-12" >
+            <center><img src="http://placehold.it/350x150"></center>
+            <center><p>Activity 4</p></center>
+        </div>
+
+        <div class="columns large-4 medium-4 small-12">
+            <center><img src="http://placehold.it/350x150"></center>
+            <center><p>Activity 5</p></center>
+        </div>
+        <div class="columns large-4 medium-4 small-12">
+            <center><img src="http://placehold.it/350x150"></center>
+            <center><p>Activity 6</p></center>
+        </div>
+
+            </div>
+        <div class="row" >
+            <div class="columns large-4 medium-4 small-12" >
+                <center><img src="http://placehold.it/350x150"></center>
+                <center><p>Activity 7</p></center>
+            </div>
+
+            <div class="columns large-4 medium-4 small-12">
+                <center><img src="http://placehold.it/350x150"></center>
+                <center><p>Activity 8</p></center>
+            </div>
+            <div class="columns large-4 medium-4 small-12">
+                <center><img src="http://placehold.it/350x150"></center>
+                <center><p>Activity 9</p></center>
+            </div>
+            </div>
 <!-- ADD ANY SCRIPTS HERE -->
 </body>
 </html>
