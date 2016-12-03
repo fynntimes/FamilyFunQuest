69b66c054f3f6e485a4d92509c1da6ceb033e69f
idharamsi <idharamsi@gmail.com>
2016-12-03 17:21:38 -0500

    Started to revise tickets form.
    Started to revise account creation form.

--- account/login/signup.php
+++ account/login/signup.php
@@ -17,9 +17,99 @@
     <!-- Bootstrap -->
     <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="../css/main.css" rel="stylesheet" media="screen">
+
+      <style>
+          @media(min-width:527px) {
+              .container-fluid {
+                  padding: 10px;
+                  background-color: gainsboro;
+                  margin: auto;
+                  width: 60%;
+              }
+
+          }
+
+          p{
+              color:black;
+          }
+      </style>
   </head>
 
   <body>
+  <section class="section section-padded">
+  <div class="container-fluid">
+      <form>
+          <div class="col-md-6">
+              <div class="form-group">
+                  <label for="name" class="control-label">Full Name</label>
+                  <input type="text" class="form-control" placeholder="Full name">
+
+              </div>
+          </div>
+          <div class="col-md-6">
+              <div class="form-group"> <!-- Email field !-->
+                  <label for="email_id" class="control-label">Email</label>
+                  <input type="email" class="form-control" id="email_id" name="email_name"
+                         placeholder="name@domain.com">
+              </div>
+          </div>
+
+          <div class="col-md-6">
+              <p>Do you want to receive promotional emails?</p>
+              <div class="radio">
+                  <label>
+                      <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                      Yes
+                  </label>
+              </div>
+              <div class="radio">
+                  <label>
+                      <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+                      No
+                  </label>
+              </div>
+          </div>
+
+
+          <div class="col-md-6">
+              <div class="form-group">
+                  <label for="phone" class="control-label">Phone Number</label>
+                  <input type="text" class="form-control" placeholder="(123) 456-789">
+              </div>
+          </div>
+
+          <div class="col-md-6">
+              <div class="form-group">
+                  <label for="creditcard" class="control-label">Credit Card Number</label>
+                  <input type="text" class="form-control" placeholder="Last 4 Digits">
+              </div>
+          </div>
+
+          <div class="col-md-6">
+              <div class="form-group">
+                  <label for="expiry" class="control-label">Expiration Date</label>
+                  <input type="text" class="form-control" placeholder="mm/dd/yyyy">
+              </div>
+          </div>
+
+          <div class="col-md-6">
+              <div class="form-group">
+                  <label for="password" class="control-label">Create a Password</label>
+                  <input type="text" class="form-control" placeholder="Password">
+              </div>
+          </div>
+
+          <div class="col-md-6">
+              <div class="form-group">
+                  <label for="password" class="control-label">Repeat Password</label>
+                  <input type="text" class="form-control" placeholder="Password">
+              </div>
+          </div>
+
+
+
+  </div>
+  </section>
     <div class="container">
 
       <form class="form-signup" id="usersignup" name="usersignup" method="post" action="createuser.php">
