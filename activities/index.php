9d7066884f5f5fd8cf99ee3bc84f510f8a28daca
SirFaizdat <fdatoo7@gmail.com>
2016-12-04 21:27:14 -0500

    Book ticket button on Activities page functional

--- activities/index.php
+++ activities/index.php
@@ -37,9 +37,9 @@
     <link rel="stylesheet" type="text/css" href="../css/main.css">
 
     <style>
-        .logo{
-            width:50px;
-            height:50px;
+        .logo {
+            width: 50px;
+            height: 50px;
         }
     </style>
 </head>
@@ -59,7 +59,8 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="../img/compass_rose_bw_144247.png" class="logo" data-active-url="../img/compass_rose_bw_1442472.png" alt=""></a>
+            <a class="navbar-brand" href="#"><img src="../img/compass_rose_bw_144247.png" class="logo"
+                                                  data-active-url="../img/compass_rose_bw_1442472.png" alt=""></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -83,61 +84,84 @@
 
     <div class="container">
         <h1>Our Activities</h1>
-        <div class="row" >
-            <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://farm5.staticflickr.com/4139/4906597504_d3d1ee9274.jpg" width="350" height="200"></center>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://farm5.staticflickr.com/4139/4906597504_d3d1ee9274.jpg" width="350"
+                             height="200"></center>
                 <center><h3>International Arcade</h3></center>
                 <center><p>An arcade with games from countries around the world!</p></center>
             </div>
 
-             <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://www.amerizyn.com/images/product_images/in-out-jungleadv_lg.jpg" width="350" height="200"></center>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://www.amerizyn.com/images/product_images/in-out-jungleadv_lg.jpg" width="350"
+                             height="200"></center>
                 <center><h3>Jungle Jump</h3></center>
-                 <center><p>Get wild and jump around!</p></center>
+                <center><p>Get wild and jump around!</p></center>
             </div>
-             <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://www.lmc.edu/_images/column-images/outdoor1.jpg" width="350" height="200"></center>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://www.lmc.edu/_images/column-images/outdoor1.jpg" width="350" height="200">
+                </center>
                 <center><h3>Everest Mountain Climb</h3></center>
-                 <center><p>Rock climbing with a mountain theme.</p></center>
+                <center><p>Rock climbing with a mountain theme.</p></center>
             </div>
         </div>
-        <div class="row" >
-            <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://www.themeparkreview.com/forum/files/iaapa69_331.jpg" width="350" height="200"></center>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://www.themeparkreview.com/forum/files/iaapa69_331.jpg" width="350" height="200">
+                </center>
                 <center><h3>Wild Drop Tower</h3></center>
                 <center><p>Jungle-themed speed drop tower.</p></center>
             </div>
 
-             <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://unitedsocialsports.com/wp-content/uploads/2016/04/skee-ball.jpg" width="350" height="200"></center>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://unitedsocialsports.com/wp-content/uploads/2016/04/skee-ball.jpg" width="350"
+                             height="200"></center>
                 <center><h3>Antarctic Skee Ball</h3></center>
-                 <center><p>Arctic themed game.</p></center>
+                <center><p>Arctic themed game.</p></center>
             </div>
-             <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://www.1001-votes.com/vote/1234fonds/flous-1327183861959-u.jpg" width="350" height="200"></center>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://www.1001-votes.com/vote/1234fonds/flous-1327183861959-u.jpg" width="350"
+                             height="200"></center>
                 <center><h3>Gold Rush</h3></center>
-                 <center><p>You and a group of miners venture off to get the most amount of gold!</p></center>
+                <center><p>You and a group of miners venture off to get the most amount of gold!</p></center>
             </div>
 
         </div>
-        <div class="row" >
-            <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="https://c1.staticflickr.com/1/254/521815653_05fbcd21f0.jpg" width="350" height="200"></center>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="https://c1.staticflickr.com/1/254/521815653_05fbcd21f0.jpg" width="350" height="200">
+                </center>
                 <center><h3>Treasure Island</h3></center>
                 <center><p>Deep sea themed ball pit.</p></center>
             </div>
 
-             <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://www.richardsonadventurefarm.com/-Images/_ResponsiveSite/_FormPhotos/CornMaze.jpg" width="350" height="200"></center>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img
+                        src="http://www.richardsonadventurefarm.com/-Images/_ResponsiveSite/_FormPhotos/CornMaze.jpg"
+                        width="350" height="200"></center>
                 <center><h3>Maize Maze</h3></center>
-                 <center><p>Find your way out of a labyrinth of corn!</p></center>
+                <center><p>Find your way out of a labyrinth of corn!</p></center>
             </div>
-             <div class="col-lg-4 col-md-4 col-sm-12" >
-                <center><img src="http://www.portofinoinnanaheim.com/img/slides/anaheim-ice1.jpg"width="350" height="200"></center>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center><img src="http://www.portofinoinnanaheim.com/img/slides/anaheim-ice1.jpg" width="350"
+                             height="200"></center>
                 <center><h3>Polar skating</h3></center>
-                 <center><p>Slide around on the ice caps!</p></center>
-                 <center><button type="button" class="btn btn-blue">Book your tickets!</button></center>
+                <center><p>Slide around on the ice caps!</p></center>
             </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <center>
+                    <a href="/tickets"><button type="button" class="btn btn-blue">Book your tickets!</button></a>
+                </center>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+            </div>
+
         </div>
     </div>
 
